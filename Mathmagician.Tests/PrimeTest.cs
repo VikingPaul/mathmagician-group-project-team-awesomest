d6dfb8d308b9953b6562bf90c1d26b7a941e684f
VikingPaul <wip321@yahoo.com>
2016-08-03 20:13:40 -0500

    test typeof

--- Mathmagician.Tests/PrimeTest.cs
+++ Mathmagician.Tests/PrimeTest.cs
@@ -15,12 +15,17 @@ namespace Mathmagician.Tests
         [TestMethod]
         public void Prime1()
         {
+            Assert.IsInstanceOfType(new Prime(1).PrimeList(), typeof(List<int>));
+        }
+        [TestMethod]
+        public void Prime2()
+        {
             Prime num = new Prime(1);
             List<int> expected = new List<int> { 2 };
             Assert.AreEqual(expected.ToString(), num.PrimeList().ToString());
         }
         [TestMethod]
-        public void Prime2()
+        public void Prime3()
         {
             Prime num = new Prime(5);
             List<int> expected = new List<int> { 2, 3, 5, 7, 11 };

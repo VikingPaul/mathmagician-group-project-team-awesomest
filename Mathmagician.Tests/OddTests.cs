d76fd8037e255bd6ecb6586ed6f7172907d7881c
Bernard Anderson <bernardanderson@gmail.com>
2016-08-06 10:16:46 -0500

    Added one additional Odd test for the length

--- Mathmagician.Tests/OddTests.cs
+++ Mathmagician.Tests/OddTests.cs
@@ -7,6 +7,7 @@ namespace Mathmagician.Tests
     [TestClass]
     public class OddTests
     {
+        // Checks to see that Odd class isn't null
         [TestMethod]
         public void Odd_EnsureICanCreateAnInstance()
         {
@@ -14,10 +15,11 @@ namespace Mathmagician.Tests
 
             //act (nothing in this case)
 
-            //assert (checks to see that Odd class isn't null)
+            //assert
             Assert.IsNotNull(new Odd());
         }
 
+        // Makes a new instance of Odd, runs the CountOddNumbers method with "4" as the input and looks to see if a List<int> is returned
         [TestMethod]
         public void Odd_IsAListReturned()
         {
@@ -25,11 +27,11 @@ namespace Mathmagician.Tests
 
             //act (nothing in this case)
 
-            //assert (makes a new instance of Odd, runs the CountOddNumbers method with "4" as the input and looks to see 
-            //        if a List<int> is returned)
+            //assert
             Assert.IsInstanceOfType(new Odd().CountOddNumbers(4), typeof(List<int>));
         }
 
+        // Makes a new instance of Odd, runs the CountOddNumbers method with "15" as the input and looks at compares element 14 to what the 14th odd number should be
         [TestMethod]
         public void Odd_IsTheFifteenthNumberCorrect()
         {
@@ -37,11 +39,11 @@ namespace Mathmagician.Tests
 
             //act (nothing in this case)
 
-            //assert (makes a new instance of Odd, runs the CountOddNumbers method with "15" as the input and looks at
-            //        compares element 14 to what the 14th odd number should be)
+            //assert
             Assert.AreEqual(29, new Odd().CountOddNumbers(15)[14]);
         }
 
+        // Makes a new instance of Odd, runs the CountOddNumbers method with "10000" as the input and looks at compares element 14 to what the 14th odd number should be
         [TestMethod]
         public void Odd_IsTheFiveThousanthNumberCorrect()
         {
@@ -49,9 +51,20 @@ namespace Mathmagician.Tests
 
             //act (nothing in this case)
 
-            //assert (makes a new instance of Odd, runs the CountOddNumbers method with "10000" as the input and looks at
-            //        compares element 14 to what the 14th odd number should be)
+            //assert 
             Assert.AreEqual(9999, new Odd().CountOddNumbers(10000)[4999]);
+        }
+
+        // Checks to make sure the correct number of numbers are being reported
+        [TestMethod]
+        public void Odd_ReturnedListLength()
+        {
+            //arrange (nothing in this case)
+
+            //act (nothing in this case)
+
+            //assert 
+            Assert.AreEqual(10000, new Odd().CountOddNumbers(10000).Count);
         }
     }
 }
